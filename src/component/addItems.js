1901a3e090594a98230b212ece07fef18b6bd6a7
Saro9033 <ssaravanan2310@gmail.com>
2023-07-30 21:28:18 +0530

    last

--- src/component/addItems.js
+++ src/component/addItems.js
@@ -4,7 +4,7 @@ const addItems = ({ handleSubmit, addItems, setAddItems}) => {
   return (
            
            <form className='addForm' onSubmit={handleSubmit}>
-        <h3>To-Do List Add Items</h3>
+        <h3>ToDo List Add Items</h3>
             <input required placeholder='Add Items' type='text' value={addItems} autoFocus 
              onChange={(e)=> setAddItems(e.target.value)}></input>
             <button className='search' type='submit' >submit</button></form>
